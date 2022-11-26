0a60d16d7e99d3ad68278c6e31dffabae04d2a22
keyiflerolsun <keyiflerolsun@gmail.com>
2022-11-27 01:37:43 +0300

    🕊
    🕊

--- Kekik/proxy_ver.py
+++ Kekik/proxy_ver.py
@@ -5,7 +5,7 @@ from Kekik   import dosya2set
 from aiohttp import BasicAuth
 from random  import shuffle, choice
 
-def proxy_ver(proxi_txt:str, tur=Literal["requests", "aiohttp", "selenium"]) -> None | dict[str, str] | ((str and BasicAuth) or (str and None)) | str:
+def proxy_ver(proxi_txt:str, tur=Literal["requests", "aiohttp", "selenium", "httpx"]) -> None | dict[str, str] | ((str and BasicAuth) or (str and None)) | str:
 
     proxiler = list(dosya2set(proxi_txt)) if dosya2set(proxi_txt) else None
 
@@ -26,24 +26,36 @@ def proxy_ver(proxi_txt:str, tur=Literal["requests", "aiohttp", "selenium"]) -> 
             proxi_auth = BasicAuth(p_user, p_pass, "utf-8")
 
             requests_proxi = {
-                'http'   : f'http://{p_user}:{p_pass}@{p_ip}:{p_port}',
-                'https'  : f'http://{p_user}:{p_pass}@{p_ip}:{p_port}',
-                # 'socks5' : f'socks5://{p_user}:{p_pass}@{p_ip}:{p_port}',
+                "http"   : f"http://{p_user}:{p_pass}@{p_ip}:{p_port}",
+                "https"  : f"http://{p_user}:{p_pass}@{p_ip}:{p_port}",
+                # "socks5" : f"socks5://{p_user}:{p_pass}@{p_ip}:{p_port}",
+            }
+
+            httpx_proxi = {
+                "http://"  : f"http://{p_user}:{p_pass}@{p_ip}:{p_port}",
+                "https://" : f"http://{p_user}:{p_pass}@{p_ip}:{p_port}"
             }
         case 2:
             p_ip, p_port = proxi_part
             proxi_auth   = None
 
             requests_proxi = {
-                'http'   : f'http://{p_ip}:{p_port}',
-                'https'  : f'http://{p_ip}:{p_port}',
-                # 'socks5' : f'socks5://{p_ip}:{p_port}',
+                "http"   : f"http://{p_ip}:{p_port}",
+                "https"  : f"http://{p_ip}:{p_port}",
+                # "socks5" : f"socks5://{p_ip}:{p_port}",
+            }
+
+            httpx_proxi = {
+                "http://"  : f"http://{p_ip}:{p_port}",
+                "https://" : f"http://{p_ip}:{p_port}"
             }
         case _:
             return None
 
     match tur:
         case "aiohttp":
-            return f'http://{p_ip}:{p_port}', proxi_auth
+            return f"http://{p_ip}:{p_port}", proxi_auth
         case "requests":
             return requests_proxi
+        case "httpx":
+            return httpx_proxi
