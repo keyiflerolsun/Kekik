171a1a23e5ab109ba54e3d0dea3888bdf2e09eac
keyiflerolsun <keyiflerolsun@gmail.com>
2023-03-11 23:07:10 +0300

    🕊
    🕊

--- Kekik/dict2csv.py
+++ Kekik/dict2csv.py
@@ -7,8 +7,8 @@ def dict2csv(dosya_adi:str, veriler:list[dict]) -> bool:
     # * CSV Kaydet
     dizin = "/".join(dosya_adi.split("/")[:-1]) + "/"
     Path(dizin).mkdir(parents=True, exist_ok=True)
-    with open(dosya_adi, "w+") as dosya:
-        csv = DictWriter(dosya, fieldnames=veriler[0].keys())
+    with open(dosya_adi, "w+", encoding="utf-8") as dosya:
+        csv = DictWriter(dosya, fieldnames=veriler[0].keys(), lineterminator="\n")
         csv.writeheader()
         for veri in veriler:
             csv.writerow(veri)
