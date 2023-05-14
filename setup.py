3747f81e243eeb777aaae821c26d9c0a3d59df2d
keyiflerolsun <keyiflerolsun@gmail.com>
2023-05-14 10:04:41 +0300

    🕊
    🕊

--- setup.py
+++ setup.py
@@ -4,30 +4,28 @@ from setuptools import setup
 from io         import open
 
 setup(
-    author       = "keyiflerolsun",
-    author_email = "keyiflerolsun@gmail.com",
-
-    packages     = ["Kekik"],
-
+    # ? Genel Bilgiler
     name         = "Kekik",
-    version      = "1.2.9",
+    version      = "1.3.0",
     url          = "https://github.com/keyiflerolsun/Kekik",
     description  = "İşlerimizi kolaylaştıracak fonksiyonların el altında durduğu kütüphane..",
     keywords     = ["Kekik", "KekikAkademi", "keyiflerolsun"],
 
-    long_description_content_type = "text/markdown",
-    long_description              = "".join(open("README.md", encoding="utf-8").readlines()),
-    include_package_data          = True,
+    author       = "keyiflerolsun",
+    author_email = "keyiflerolsun@gmail.com",
 
-    license     = "GPLv3+",
-    classifiers = [
+    license      = "GPLv3+",
+    classifiers  = [
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Programming Language :: Python :: 3"
     ],
 
+    # ? Paket Bilgileri
+    packages         = ["Kekik"],
     python_requires  = ">=3.10",
     install_requires = [
+        "pip",
         "setuptools",
         "wheel",
         "pytz",
@@ -46,5 +44,10 @@ setup(
         "Pillow",
         "notify-py",
         # "py3-validate-email"
-    ]
+    ],
+
+    # ? PyPI Bilgileri
+    long_description_content_type = "text/markdown",
+    long_description              = "".join(open("README.md", encoding="utf-8").readlines()),
+    include_package_data          = True
 )
